ebf9094f10b30ef7a3668260f11b114d49f62b4a
wes337 <wesmoses@gmail.com>
2022-09-27 20:38:21 +0300

    add clear search button

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDebouncedState } from "@mantine/hooks";
-import { TextInput } from "@mantine/core";
-import { IconSearch } from "@tabler/icons";
+import { TextInput, ActionIcon } from "@mantine/core";
+import { IconSearch, IconX } from "@tabler/icons";
 import { useAppDispatch } from "../app/hooks";
 import { setSearchQuery } from "../app/slice";
 
 export const SearchInput = () => {
+  const ref = useRef<HTMLInputElement>(null);
   const [debouncedSearchQuery, setDebouncedSearchQuery] = useDebouncedState(
     "",
     200
@@ -16,12 +17,27 @@ export const SearchInput = () => {
     dispatch(setSearchQuery(debouncedSearchQuery));
   }, [debouncedSearchQuery, dispatch]);
 
+  const clearSearchQuery = () => {
+    setDebouncedSearchQuery("");
+    if (ref.current) {
+      ref.current.value = "";
+    }
+  };
+
   return (
     <TextInput
+      ref={ref}
       icon={<IconSearch />}
       defaultValue={debouncedSearchQuery}
       placeholder="Country name"
       onChange={(event) => setDebouncedSearchQuery(event.currentTarget.value)}
+      rightSection={
+        debouncedSearchQuery && (
+          <ActionIcon onClick={clearSearchQuery}>
+            <IconX size={18} />
+          </ActionIcon>
+        )
+      }
     />
   );
 };
